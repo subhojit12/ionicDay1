92bccae2522bbc588eee8b01c379f8b6ed261b9b
Admin <admin@Admins-MBP.Dlink>
2019-03-19 11:44:49 +0530

    android platform added

---
+++ src/app/tab4/tab4.module.ts
@@ -0,0 +1,17 @@
+import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Tab4Component } from './tab4.component';
+
+@NgModule({
+  imports: [
+    IonicModule,
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild([{ path: '', component: Tab4Component }])
+  ],
+  declarations: [Tab4Component]
+})
+export class Tab4Module {}
